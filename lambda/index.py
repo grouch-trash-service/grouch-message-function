a4b7ce04c29b8e48ba3f33b18a8f281521a1e409
Matt Porter <matthew.js.porter@gmail.com>
2020-06-19 23:18:23 -0400

    switching from python to spring-boot function

--- lambda/index.py
+++
@@ -1,9 +0,0 @@
-import json
-def handler(event, context):
-    return {
-        "statusCode": 200,
-        "headers": {'Access-Control-Allow-Origin': '*'},
-        "body": json.dumps({
-            "text": "Hello. Trash pickup will be on Tuesday... Now SCRAM!",
-        }),
-    }
