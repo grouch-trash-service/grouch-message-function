76dc560c96f081039a04dcf9bbe2cb16f8052a29
Matt Porter <matthew.js.porter@gmail.com>
2020-09-08 09:44:09 -0400

    removing unused logger from MessageService

--- src/main/java/grouch/message/provider/MessageService.java
+++ src/main/java/grouch/message/provider/MessageService.java
@@ -1,13 +1,10 @@
 package grouch.message.provider;
 
 import org.openapitools.model.Message;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MessageService implements MessageProvider {
-    private final Logger log = LoggerFactory.getLogger(MessageService.class);
 
     public Message getMessage() {
         return new Message().text("Trash Pickup is on Tuesday...Now Scram!");
