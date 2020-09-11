0abd566e50dfb0c8834c6ad3ff1b61d94323199b
Matt Porter <matthew.js.porter@gmail.com>
2020-09-10 23:09:41 -0400

    adding lombok for better code coverage

--- src/main/java/grouch/message/model/Message.java
+++ src/main/java/grouch/message/model/Message.java
@@ -1,50 +1,15 @@
 package grouch.message.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-import java.util.Objects;
-
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Message {
 
     @JsonProperty("text")
     private String text;
-
-    public Message(final String text) {
-        this.text = text;
-    }
-
-    public Message() {
-    }
-
-    public String getText() {
-        return text;
-    }
-
-    public void setText(final String text) {
-        this.text = text;
-    }
-
-    @Override
-    public boolean equals(final Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        Message message = (Message) o;
-        return Objects.equals(text, message.text);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(text);
-    }
-
-    @Override
-    public String toString() {
-        return "Message{"
-                + "text='" + text + '\''
-                + '}';
-    }
 }
