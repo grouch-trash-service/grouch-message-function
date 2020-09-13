3312a64b6a3aaa85c0fc95a867435bfd397877d3
Matt Porter <matthew.js.porter@gmail.com>
2020-09-13 15:03:05 -0400

    Using strict Cucumber Tests

--- src/test/java/grouch/message/cucumber/CucumberRunner.java
+++ src/test/java/grouch/message/cucumber/CucumberRunner.java
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = {"pretty"}, features = "src/test/resources/features")
+@CucumberOptions(plugin = {"pretty"}, features = "src/test/resources/features", strict = true)
 @SpringBootApplication
 public class CucumberRunner {
 }
