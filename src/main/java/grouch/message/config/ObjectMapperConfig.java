12adbf4172c4cf3434d368f57f10937f700d45ef
Matt Porter <matthew.js.porter@gmail.com>
2020-09-14 14:23:15 -0400

    using Configuration annotation instead of Compoment fpr ObjectMapperConfig

--- src/main/java/grouch/message/config/ObjectMapperConfig.java
+++ src/main/java/grouch/message/config/ObjectMapperConfig.java
@@ -2,9 +2,9 @@ package grouch.message.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
-import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.Configuration;
 
-@Component
+@Configuration
 public class ObjectMapperConfig {
     @Bean
     public ObjectMapper objectMapper() {
